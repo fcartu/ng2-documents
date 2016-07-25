9dba350957a717e3c24d66f3444aba89d153f271
fcartu <fcartu@hotmail.com>
2016-07-25 17:38:25 -0500

    fix router-oulet bug

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'ng2-docs',
   pipes: [],
   providers: [],
   directives: [ ROUTER_DIRECTIVES ],
+  precompile: [HomeComponent],
   templateUrl: './app.html',
 })
 export class App {
